e6d361ad674d95b9ed479fee54e4f7eb12ecdd0f
Zain-Asif-Falcon <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2023-06-09 11:58:27 +0500

    Update Footsize.py

--- Footsize.py
+++ Footsize.py
@@ -11,7 +11,6 @@ model = torch.hub.load('ultralytics/yolov5', 'custom', path=r"PATH", force_reloa
 model1 = tf.keras.models.load_model(r"C:\Users\Falcon\Downloads\size.h5")
 
 
-
 from tkinter import *
 root=Tk()
 import PIL
@@ -22,14 +21,8 @@ root.config(bg="#00406c")
 # img = Image.open("path/to/image")
 pic=PhotoImage(file=r"PATH")
 
-
-
 frm=Label(root,image=pic,bg="#00406c")
 frm.place(x=740,y=190)
-
-
-
-
 
 
 l1=Label(frm,text='Welcome to FootXacles',font= ('Helvetica 15 bold'),bg="#00406c",fg="white")
@@ -38,37 +31,6 @@ l1=Label(frm,text='by Falcon IT',bg="#00406c",fg="white")
 l1.place(x=150,y=170)
 
 # ""
-
-
-pic1=PhotoImage(file=r"E:\muneebfot\yolov5\icons8-next-page-64.png")
-b1=PhotoImage(file=r"E:\muneebfot\yolov5\icons8-web-camera-48.png")
-b2=PhotoImage(file=r"E:\muneebfot\yolov5\icons8-image-gallery-48.png")
-
-b11=PhotoImage(file=r"C:\Users\Falcon\Downloads\icons8-foot-64.png")
-b22=PhotoImage(file=r"C:\Users\Falcon\Downloads\icons8-trainers-64.png")
-
-def size(file_path):
-    path = file_path
-    img = tf.keras.utils.load_img(
-        path, target_size=(224, 224)
-    )
-    img_array = tf.keras.utils.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0) # Create a batch
-
-    predictions = model1.predict(img_array)
-    score = tf.nn.softmax(predictions[0])
-    class_names = ['10', '11', '8', '9']
-
-
-    print(
-            "This image most likely belongs to {} with a {:.2f} percent confidence."
-            .format(class_names[np.argmax(score)], 100 * np.max(score)))
-    size11=Label(root, text="This image most likely belongs to {} \n with a {:.2f} percent confidence."
-            .format(class_names[np.argmax(score)], 100 * np.max(score)),font= ('Helvetica 18 bold'),bg="#00406c",fg="white")
-    size11.place(x=1160,y=340)
-
-
-
 
 
 def camera():
@@ -156,32 +118,10 @@ def fun():
         bb21.place(x=230,y=240)
     frm.destroy()
     
-    frm11=Label(root,image=pic,bg="#00406c")   
-    back1=Button(frm11,image=bk,borderwidth=0,command=back,bg="#00406c")
-    back1.place(x=80,y=80)
-    frm11.place(x=740,y=190)
-    m =Label(frm11,text='MENU',font= ('Helvetica 25 bold'),bg="#00406c",fg='white')
-    m.place(x=140,y=140)
-    bb1=Button(frm11,image=b1,borderwidth=0,bg="#00406c",command=fun_2)
-    bb1.place(x=100,y=240)
-    bb2=Button(frm11,image=b2,borderwidth=0,bg="#00406c",command=fun_3
-               )
-    bb2.place(x=230,y=240)
-
-
-
    
-
-
 
 
 frm1=Button(frm,image=pic1,borderwidth=0,command=fun,bg="#00406c")
 frm1.place(x=160,y=400)
-
-
-
-
-
-
 
 root.mainloop()
